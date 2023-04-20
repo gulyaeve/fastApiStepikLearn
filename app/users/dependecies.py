f8a0f246dd37713b68e98a9dff0000242907c4e1
gulyaeve <gulyaev.e@icloud.com>
2023-04-20 21:27:57 +0300

    added booking endpoint for user id

---
+++ app/users/dependecies.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from fastapi import Request, HTTPException, status, Depends
+from jose import jwt, JWTError
+
+from app.config import settings
+from app.users.dao import UsersDAO
+
+
+def get_token(request: Request):
+    token = request.cookies.get("booking_access_token")
+    if not token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Empty token")
+    return token
+
+
+async def get_current_user(token: str = Depends(get_token)):
+    expire: str = ""
+    user_id: str = ""
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, settings.ALGORITHM
+        )
+        expire = payload.get("exp")
+        user_id = payload.get("sub")
+    except JWTError:
+        HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+    if not expire or (int(expire) < datetime.utcnow().timestamp()):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Expired token")
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Empty sub")
+
+    user = await UsersDAO.find_by_id(int(user_id))
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    return user
