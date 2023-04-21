008d61761fc55563e632af73ee0e1596bae3e4d5
gulyaeve <gulyaev.e@icloud.com>
2023-04-21 11:24:41 +0300

    added user logout and get_me

--- app/users/router.py
+++ app/users/router.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, HTTPException, status, Response
+from fastapi import APIRouter, HTTPException, status, Response, Depends
 
 from app.users.auth import get_password_hash, authenticate_user, create_access_token
 from app.users.dao import UsersDAO
+from app.users.dependecies import get_current_user
 from app.users.models import Users
 from app.users.schemas import SUserAuth
 
@@ -30,3 +31,13 @@ async def login_user(response: Response, user_data: SUserAuth):
     )
     response.set_cookie("booking_access_token", access_token, httponly=True)
     return {"access_token": access_token}
+
+
+@router.post("/logout")
+async def logout_user(response: Response):
+    response.delete_cookie("booking_access_token")
+
+
+@router.get("/me")
+async def read_users_me(current_user: Users = Depends(get_current_user)):
+    return current_user
