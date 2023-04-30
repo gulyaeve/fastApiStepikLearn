e05828ffb4cb05a0f17d769c6a4b210ba281083f
gulyaeve <gulyaev.e@icloud.com>
2023-04-30 11:47:31 +0300

    Added templates and images

--- app/main.py
+++ app/main.py
@@ -1,17 +1,24 @@
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.users.router import router as users_router
 from app.bookings.router import router as booking_router
 from app.hotels.router import router as hotels_router
 from app.hotels.rooms.router import router as rooms_router
+from app.pages.router import router as pages_router
+from app.images.router import router as images_router
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(users_router)
 app.include_router(booking_router)
 app.include_router(hotels_router)
 app.include_router(rooms_router)
+app.include_router(pages_router)
+app.include_router(images_router)
 
 
 # @app.get("/")
