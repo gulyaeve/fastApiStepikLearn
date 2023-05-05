3a85f871533d04af01a0ef9b94d20a82ad001c48
gulyaeve <gulyaev.e@icloud.com>
2023-05-05 21:23:42 +0300

    Added tests

--- app/config.py
+++ app/config.py
@@ -1,7 +1,11 @@
+from typing import Literal
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+
     # Postgres
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
@@ -9,10 +13,24 @@ class Settings(BaseSettings):
     POSTGRES_PORT: int
     POSTGRES_DB: str
 
+    # Test database
+    TEST_POSTGRES_USER: str
+    TEST_POSTGRES_PASSWORD: str
+    TEST_POSTGRES_HOST: str
+    TEST_POSTGRES_PORT: int
+    TEST_POSTGRES_DB: str
+
     @property
     def database_url(self):
         user = f"{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
         database = f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return f'postgresql+asyncpg://{user}@{database}'\
+
+
+    @property
+    def test_database_url(self):
+        user = f"{self.TEST_POSTGRES_USER}:{self.TEST_POSTGRES_PASSWORD}"
+        database = f"{self.TEST_POSTGRES_HOST}:{self.TEST_POSTGRES_PORT}/{self.TEST_POSTGRES_DB}"
         return f'postgresql+asyncpg://{user}@{database}'
 
     # Redis
--- app/hotels/rooms/models.py
+++ app/hotels/rooms/models.py
@@ -10,9 +10,9 @@ class Rooms(Base):
     id = Column(Integer, primary_key=True)
     hotel_id = Column(ForeignKey("hotels.id"))
     name = Column(String, nullable=False)
-    description = Column(String, nullable=False)
+    description = Column(String, nullable=True)
     price = Column(Integer, nullable=False)
-    services = Column(JSON, nullable=False)
+    services = Column(JSON, nullable=True)
     quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
 
