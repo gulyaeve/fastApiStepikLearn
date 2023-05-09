31ff12e6e22d0c13fcc9762aa12351b277bc43f2
gulyaeve <gulyaev.e@icloud.com>
2023-05-09 21:02:00 +0300

    Added bookings delete api tests

---
+++ app/tests/integration_tests/test_bookings/delete_bookings_api_tests.py
@@ -0,0 +1,22 @@
+from httpx import AsyncClient
+import pytest
+
+
+@pytest.mark.parametrize("status_code", [
+    200,
+])
+async def test_get_and_delete_booking(
+    status_code,
+    authenticated_ac: AsyncClient,
+):
+    response = await authenticated_ac.get("/bookings/get")
+    print(response.json())
+    assert response.status_code == status_code
+
+    for booking in response.json():
+        await authenticated_ac.delete(f"/bookings/{booking['id']}")
+
+    response = await authenticated_ac.get("/bookings/get")
+    print(response.json())
+    print(f"{len(response.json())=}")
+    assert len(response.json()) == 0
